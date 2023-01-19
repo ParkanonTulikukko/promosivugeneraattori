37fa0a75fcebd7a1d4b422269746d9f995c3a354
kcheku <heikki.kuivala@gmail.com>
2023-01-19 17:10:37 +0200

    Sovellus tekee zip-paketin käyttäjän tietojen perusteella. Paketti sisältää html-sivun ja kappaleen perusteella uudelleennimetyn mp3-tiedoston.

--- promosivugeneraattori/src/App.js
+++ promosivugeneraattori/src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg'
 import './App.css'
 import React, { useState, useEffect } from 'react'
-//import CardProfile from './player/Player.js';
+import JSZip from 'jszip';
+import { saveAs } from 'file-saver';
 
 function App() {
 
   const [kappaleenNimi, setKappaleenNimi] = useState("")
+  const [kappale, setKappale] = useState(null);
 
-  function annaFile() {
-    let doc = new DOMParser().parseFromString("<html></html>", "text/html");
+  let doc = new DOMParser().parseFromString("<html></html>", "text/html");
+
+  const lataaKappale = (e) => {
+    setKappale(e.target.files[0])
+    }
+
+  function tallennaSivu() {
 
     let head = doc.createElement("head");
     let title = doc.createElement("title");
@@ -22,28 +29,56 @@ function App() {
     body.appendChild(h1);
     doc.documentElement.appendChild(body);
 
-    let fileData = doc.documentElement.outerHTML;
+    lataaZip()
 
+    }
+
+  function lataaZip() {
+    let fileData = doc.documentElement.outerHTML;
     const blob = new Blob([fileData], { type: "text/html" });
     const url = URL.createObjectURL(blob);
+
+    const zip = new JSZip();
+    const folder = zip.folder("promosivu");
+
+    folder.file("index.html", blob)
+
+    const reader = new FileReader();
+    reader.onload = function() {
+      const mp3Blob = new Blob([reader.result], { type: 'audio/mp3' });
+      folder.file(kappaleenNimi + ".mp3", mp3Blob);
+      zip.generateAsync({ type: "blob" }).then((content) => {
+        saveAs(content, "promosivu.zip");
+        URL.revokeObjectURL(url);
+        });
+      }
+    reader.readAsArrayBuffer(kappale);
+
+    /*
     const link = document.createElement("a");
     link.download = "promosivu.html";
     link.href = url;
-    link.click(); 
-    }
-
-  function handleChange(e) {
-    setKappaleenNimi(e.target.value)
-    }
+    link.click();
+    */ 
+    }  
 
   return (
     <div className="App">
+      <p><input type="file" onChange={lataaKappale} /></p>
+      {kappale && <p>Selected file: {kappale.name}</p>}
+
+      <form>  
+      <label for="kappaleenNimi">Kappaleen nimi: </label>
         <input
+          id="kappaleenNImi"
           type="text"
           value={kappaleenNimi}
-          onChange={handleChange}
+          onChange={(e) => {setKappaleenNimi(e.target.value)}}
         />
-      <button type="button" onClick={() => annaFile()} value="tallenna">tallenna</button> 
+      </form>
+      <br/>
+      <button type="button" onClick={() => tallennaSivu()} value="tallenna">Tallenna sivu</button>
+      
     </div>
   );
 }
